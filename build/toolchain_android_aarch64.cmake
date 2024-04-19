5655c728c95923cfe7574ddf2447539200c78f55
Satoshi ARA <satoshi.a@gmail.com>
2024-04-19 15:42:49 +0900

    Trying to figure out how to build with windows.

--- build/toolchain_android_aarch64.cmake
+++ build/toolchain_android_aarch64.cmake
@@ -3,9 +3,12 @@
 
 # List of possible Android SDK paths
 set(possible_sdk_paths
-    ~/Library/Developer/Xamarin/android-sdk-macosx # Visual Studio for Mac
-    ~/Library/Android/sdk   # Android Studio (Mac)
-    ~/Android/Sdk           # Android Studio (Ubuntu Software Center)
+    ~/Library/Developer/Xamarin/android-sdk-macosx  # Mac / Visual Studio for Mac
+    ~/Library/Android/sdk                           # Mac / Android Studio
+    ~/Android/Sdk                                   # Ubuntu / Android Studio (Software Center)
+    /usr/lib/android-sdk                            # Ubuntu / apt install android-sdk
+    $ENV{ProgramFiles\(x86\)}/Android/android-sdk     # Windows / Visual Studio 2022
+    $ENV{LOCALAPPDATA}/Android/sdk                  # Windows / Android Studio
 )
 
 # Find the Android SDK path
@@ -21,26 +24,75 @@ endforeach()
 if(NOT android_sdk_path)
     message(FATAL_ERROR "Could not find Android SDK in any of the specified paths")
 else()
-    message("Android SDK path: ${android_sdk_path}")
+    message("Android SDK  : ${android_sdk_path}")
 endif()
 
 ## --------------------------------------------------------------------
 ## find android ndk
 
 # Get a list of all NDK versions
-file(GLOB ndk_versions LIST_DIRECTORIES true "${android_sdk_path}/ndk/*")
+if(EXISTS "${android_sdk_path}/ndk-bundle")
+    set(android_ndk_path "${android_sdk_path}/ndk-bundle")
+else()
+    file(GLOB ndk_versions LIST_DIRECTORIES true "${android_sdk_path}/ndk/*")
 
-# Sort the list in descending order
-list(SORT ndk_versions ORDER DESCENDING)
+    list(LENGTH ndk_versions ndk_versions_length)
+    if(ndk_versions_length EQUAL 0)
+        message(FATAL_ERROR "Could not find Android NDK in any of the specified paths")
+    endif()
 
-# Get the latest NDK version
-list(GET ndk_versions 0 android_ndk_path)
+    # Sort the list in descending order
+    list(SORT ndk_versions ORDER DESCENDING)
+
+    # Get the latest NDK version
+    list(GET ndk_versions 0 android_ndk_path)
+endif()
 
 if(NOT android_ndk_path)
     message(FATAL_ERROR "Could not find Android NDK in any of the specified paths")
 else()
-    message("Android NDK path: ${android_ndk_path}")
+    message("Android NDK  : ${android_ndk_path}")
 endif()
+
+## --------------------------------------------------------------------
+## find llvm prebuilt
+
+if(WIN32)
+    set(android_ndk_prebuilt "${android_ndk_path}/toolchains/llvm/prebuilt/windows")
+elseif(APPLE)
+    set(android_ndk_prebuilt "${android_ndk_path}/toolchains/llvm/prebuilt/darwin-x86_64")
+elseif(CMAKE_SYSTEM_NAME STREQUAL "Linux")
+    set(android_ndk_prebuilt "${android_ndk_path}/toolchains/llvm/prebuilt/linux-x86_64")
+endif()
+
+if(NOT android_ndk_prebuilt)
+    message(FATAL_ERROR "Could not find Android NDK LLVM Prebuilt in any of the specified paths")
+else()
+    message("LLVM Prebuilt: ${android_ndk_prebuilt}")
+endif()
+
+## --------------------------------------------------------------------
+## find clang
+
+# clang
+if(WIN32)
+    file(GLOB clang_versions LIST_FILES true "${android_ndk_prebuilt}/bin/aarch64-linux-android*-clang.cmd")
+else()
+    file(GLOB clang_versions LIST_FILES true "${android_ndk_prebuilt}/bin/aarch64-linux-android*-clang")
+endif()
+list(SORT clang_versions ORDER DESCENDING)
+list(GET clang_versions 0 android_clang_path)
+message("LLVM clang   : ${android_clang_path}")
+
+# clang++
+if(WIN32)
+    file(GLOB clangpp_versions LIST_FILES true "${android_ndk_prebuilt}/bin/aarch64-linux-android*-clang++.cmd")
+else()
+    file(GLOB clangpp_versions LIST_FILES true "${android_ndk_prebuilt}/bin/aarch64-linux-android*-clang++")
+endif()
+list(SORT clangpp_versions ORDER DESCENDING)
+list(GET clangpp_versions 0 android_clangpp_path)
+message("LLVM clang++ : ${android_clangpp_path}")
 
 ## --------------------------------------------------------------------
 ## toolchains
@@ -49,9 +101,9 @@ endif()
 set(CMAKE_SYSTEM_NAME Android)
 
 # specify the cross compiler
-set(CMAKE_ANDROID_NDK  ${android_ndk_path})
-set(CMAKE_C_COMPILER   ${CMAKE_ANDROID_NDK}/toolchains/llvm/prebuilt/darwin-x86_64/bin/aarch64-linux-android34-clang)
-set(CMAKE_CXX_COMPILER ${CMAKE_ANDROID_NDK}/toolchains/llvm/prebuilt/darwin-x86_64/bin/aarch64-linux-android34-clang++)
+set(CMAKE_ANDROID_NDK  "${android_ndk_path}")
+set(CMAKE_C_COMPILER   "${android_clang_path}")
+set(CMAKE_CXX_COMPILER "${android_clangpp_path}")
 
 # where is the target environment
 set(CMAKE_FIND_ROOT_PATH ${CMAKE_ANDROID_NDK})
